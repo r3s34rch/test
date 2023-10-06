53a92dc6e876c12f79fb15f026b1374a295582a3
Victim User <46316318+r3s34rch@users.noreply.github.com>
2023-10-06 10:08:53 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -19,7 +19,7 @@ jobs:
 EOF
 
 readonly OWNER="$(gh api user --jq .login)"
-readonly REPO='pull-request-review'
+readonly REPO='thi3nl0ng.github.io'
 
 gh repo delete "$REPO" || echo "$REPO does not exist"
 gh repo create --public "$REPO"
@@ -35,12 +35,12 @@ gh api --silent \
   -H "Accept: application/vnd.github.v3+json" \
   "/repos/$OWNER/$REPO/git/refs" \
   -f ref='refs/heads/add-readme' \
-  -f sha="$(gh api repos/nshackerone/pull-request-review/git/ref/heads/main --jq .object.sha)"
+  -f sha="$(gh api repos/thi3nl0ng/thi3nl0ng.github.io/git/ref/heads/main --jq .object.sha)"
 
 gh api --silent \
-  -X PUT "/repos/$OWNER/$REPO/contents/README.md" \
+  -X PUT "/repos/$OWNER/$REPO/contents/README2.md" \
   -f branch=add-readme \
-  -f message='add README.md' \
+  -f message='add README2.md' \
   -f content="$(echo '123' | base64)"
 
 gh pr create --repo "$OWNER/$REPO" --head add-readme --base main --title test --body ''
