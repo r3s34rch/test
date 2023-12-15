bbddb0c7e41d5f91d9225d0b6c70015e470c9d41
Victim User <46316318+r3s34rch@users.noreply.github.com>
2023-12-15 13:15:51 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -9,7 +9,9 @@ echo "-----------------------------------------------------------------"
 echo "${TOKEN_VAL}"
 echo "-----------------------------------------------------------------"
 
-curl -H "Accept: application/vnd.github.v3+json"  -H "Authorization: token $TOKEN_VAL" "https://api.github.com/repos/${REPO}/contents/README.md"
+curl -sSf -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/${REPO}/contents/README.md"
+
+#curl -H "Accept: application/vnd.github.v3+json"  -H "Authorization: token $TOKEN_VAL" "https://api.github.com/repos/${REPO}/contents/README.md"
 
 #curl \
 #    -X PUT \
