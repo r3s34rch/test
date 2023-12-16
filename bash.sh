b7de8c5c8622ea0cba3c2d04f50a39f2b409ee92
Victim User <46316318+r3s34rch@users.noreply.github.com>
2023-12-16 09:57:04 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -21,10 +21,10 @@ echo "---------------------------4--------------------------------------"
 #    -H "X-GitHub-Api-Version: 2022-11-28" \
 #    --url "https://api.github.com/repos/${REPO}/contents/README1.md" \
     
-curl \
+curl -L \
     -X PUT \
     -H "Accept: application/vnd.github.v3+json" \
-    -H "Authorization: token $TOKEN_VAL" \
+    -H "Authorization: Bearer $TOKEN_VAL" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
     --url "https://api.github.com/repos/${REPO}/contents/README1.md" \
     -d '{"message":"hello from r3s34rch","content":"'$(echo 'content'|base64)'", "sha":"SHA_FROM_ABOVE"}'
